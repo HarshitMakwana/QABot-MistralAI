37072c7e9e3968acde2b078b9ef6ca97936103ed
Harshit Makwana <iamharshitmakwana@gmail.com>
2024-08-01 00:07:20 +0530

    Updated config param file

--- configs/config_param.py
+++ configs/config_param.py
@@ -6,5 +6,9 @@ with open('configs/config.json', 'r') as f:
 with open('data/data.json', 'r') as f:
     data = json.load(f)
 
+new_dict = dict()
+for each in data:
+    new_dict[each.get('index')] = [each.get('short_list'),each.get('content')]
+    
 model = config_param.get("model")
 api_key = config_param.get("api_key")
